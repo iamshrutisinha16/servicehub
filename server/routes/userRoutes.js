7e44107d30824c6c358648c51b15509e5012faff
shrutisinha74168 <shrutisinha74168@gmail.com>
2026-05-10 16:41:53 +0530

    chnage in routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,8 +7,7 @@ const {
   loginUser,
 } = require("../controllers/userController");
 
-router.post("/create", createUser);
-
+router.post("/register", createUser);
 router.post("/login", loginUser);
 
 module.exports = router;
